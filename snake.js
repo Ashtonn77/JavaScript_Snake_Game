f6b54da61e6653d2fef1376532e78ab10a674e29
ashton naidoo <ashtonrbay@gmail.com>
2020-04-12 22:47:23 +0200

    added snake.js

--- snake.js
+++ snake.js
@@ -3,137 +3,152 @@ let c;
 canvas = document.querySelector('canvas');
 c = canvas.getContext('2d');
 
-let box = 32;
+let length = 30;
+let eggSize = 20;
+let v = 10;
+let snake = [];
+    snake[0] = {
+    x: 100,
+    y: 100
+}
 
-const turf = new Image();
-turf.src = "img/ground.png";
-
-const foodImg = new Image();
-foodImg.src = "img/food.png";
-
-//ausio files
+let playerScore = 0;
 let dead = new Audio();
 let eat = new Audio();
-let up = new Audio();
-let right = new Audio();
-let left = new Audio();
-let down = new Audio();
-
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-right.src = "audio/right.mp3";
-left.src = "audio/left.mp3";
-down.src = "audio/down.mp3";
+let move = new Audio();
 
 
+dead.src = "audio/crash.mp3";
+eat.src = "audio/chomp.wav";
+move.src = "audio/zoom.wav";
 
-//snake
-let snake = [];
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
+
+let newGame = false;
+function gameOver(){
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    c.fillStyle = 'white';
+    c.fillText("Game Over", canvas.width / 2 - 50, canvas.height / 2);
+    c.fillText("Click to restart", canvas.width / 2 - 50, canvas.height / 2 + 150);
+    newGame = true;
+    playerScore = 0;
+    clearInterval(game);
 }
 
-//food
-let food = {
-    x: Math.floor(Math.random() * 17 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 3) * box,
-}
 
-//score
-let score = 0;
-
-//control
-let d;
-document.addEventListener('keydown', direction);
-
-function direction(event){
-    if(event.keyCode == 37 && d != 'Right'){
-        d = 'Left';
-        left.play();
-    }else if(event.keyCode == 38 && d != 'Down'){
-        d = 'Up';
-        up.play();
-    }else if(event.keyCode == 39 && d != 'Left'){
-        d = 'Right';
-        right.play();
-    }else if(event.keyCode == 40 && d != 'Up'){
-        d = 'Down';
-        down.play();
+function collision(head, arr){
+   for(let i = 0; i < arr.length; i++){
+    if(head.x == arr[i].x && head.y == arr[i].y){
+        return true;
     }
-}
-
-
-//collision
-function collision(head,array){
-    for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
-            return true;
-        }
-    }
+   }
     return false;
 }
 
-//draw
-function draw(){
-    c.drawImage(turf, 0, 0);
-    for(let i = 0; i < snake.length; i++){
-        c.fillStyle = (i == 0) ? 'green' : 'white';
-        c.fillRect(snake[i].x, snake[i].y, box, box);
 
-        c.strokeStyle = 'red';
-        c.strokeRect(snake[i].x, snake[i].y, box, box)
-    }
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-    c.drawImage(foodImg, food.x, food.y);
 
-    //old head
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
 
+let egg = {
+    x: Math.floor(getRandom(0 + eggSize, canvas.width - eggSize)),
+    y: Math.floor(getRandom(0 + eggSize, canvas.height - eggSize))
+}
+
+function getDistance(x1, y1, x2, y2){
+    xDistance = x2 - x1;
+    yDistance = y2 - y1;
     
-    //direction
-    if(d == 'Left') snakeX -= box;
-    if(d == 'Up') snakeY -= box;
-    if(d == 'Right') snakeX += box;
-    if(d == 'Down') snakeY += box;
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
+}
+
+//direction
+let way;
+document.addEventListener('keydown', direction);
+function direction(event){
+if(event.keyCode == 37 && way != 'Right'){
+    way = 'Left';
+    move.play();
+}else if(event.keyCode == 38 && way != 'Down'){
+    way = 'Up';
+    move.play();
+}else if(event.keyCode == 39 && way != 'Left'){
+    way = 'Right';
+    move.play();
+}else if(event.keyCode == 40 && way != 'Up'){
+    way = 'Down';
+    move.play();
+}
+}
+//end direction
 
 
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
-        eat.play();
-        food = {
-            x: Math.floor(Math.random() * 17 + 1) * box,
-            y: Math.floor(Math.random() * 15 + 3) * box,
-        }
-        
-    }else{
-        //remove tail
-        snake.pop();
-    }
+let keepScore = document.querySelector('.score');
 
-      //add new head
-      let newHead = {
-        x: snakeX,
-        y: snakeY
-    }
+function draw(){   
+
+c.fillStyle = 'green';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+for(let i = 0; i < snake.length; i++){
+    c.fillStyle = (i == 0) ? 'white' : 'brown';
+c.fillRect(snake[i].x, snake[i].y, length, 30)
+}
+
+c.fillStyle = 'red';
+c.fillRect(egg.x, egg.y, eggSize, eggSize)
+
+let snake_x = snake[0].x;
+let snake_y = snake[0].y
+
+if(way == 'Left') snake_x -= v;
+if(way == 'Up') snake_y -= v
+if(way == 'Right') snake_x += v;
+if(way == 'Down') snake_y += v;
 
 
-    //gameOver
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead, snake)){
-        clearInterval(game);
-        dead.play();
-    }
+if(getDistance(snake_x, snake_y, egg.x, egg.y) <= length && getDistance(snake_x, snake_y, egg.x, egg.y) >= -length){    
+    eat.play();
+    playerScore += 5;
+    egg = {
+        x: getRandom(0 + eggSize, canvas.width - eggSize),
+        y: getRandom(0 + eggSize, canvas.height - eggSize)
+    }    
+}else{
+    snake.pop();
+}
 
-  
-    snake.unshift(newHead);
 
-    //score label
-    c.fillStyle = 'white';
-    c.font = "45px monospace"
-    c.fillText(score, 2 * box, 1.6 * box);
+let newHead = {
+    x:snake_x,
+    y:snake_y
+}
+
+if(snake_x > canvas.width || snake_x < 0 || snake_y < 0 || snake_y > canvas.height || collision(newHead, snake)){
+    dead.play();
+    gameOver();
+}
+
+snake.unshift(newHead)
+
+if(playerScore > 5 && playerScore % 50 == 0) {
+    v = 20;
+}
+
+keepScore.innerHTML = playerScore;
+
+}
+
+document.addEventListener('mousedown', restart);
+function restart(){
+   if(newGame){
+    location.reload();
+   }
 }
 
 let game = setInterval(draw, 100);
+
 
